47c0795378a30ee67c0f8326da9286bdd2559b32
apichai-Sx <kata.apex7@gmail.com>
2026-02-22 19:56:05 +0700

    the header added

--- debug.js
+++ debug.js
@@ -15,7 +15,8 @@ const Xrqust = () =>{
 	xhr.onload = () =>{
 		alert(xhr.responseText)
 	}
-	xhr.open("GET",host + "/gen-sx")
+	xhr.setRequestHeader('Origin','https://apichai-sx.github.io')
+	xhr.open("GET",host + "/gen-sx/")
 	xhr.send()
 }
 repeat()
